7b3c9220214f14d238dad930d4738eb587d9db8f
Sachin Kumar <sk92907@gmail.com>
2019-12-15 21:01:31 +0000

    Refer to readme inside v4.

---
+++ v4/README.md
@@ -0,0 +1,55 @@
+==========v1==========	
+
+1. node -init  
+2. Install express and ejs.  
+3. Add route for landing page and the campgrounds page.  
+4. Layout and Basic Styling  
+	4.1. Create Header and Footer partials  
+	4.2. Add Bootstrap  
+5. Creating new Campgrounds  
+	5.1. Setup new Campground POST route  
+	5.2. Add in body-parse (npm install body-parser --save)  
+	5.3. Setup Route to show form  
+	5.4. Add basic unstyled form  
+6. Style the Campgrounds Page  
+	6.1. Add a better header/title  
+	6.2. Make campgrounds display in a grid  
+7. Style the navbar and Form  
+	7.1. Add a navbar to all templates  
+	7.2. Style the new campground form  
+
+==========v2==========	
+  
+8. Add Mongoose  
+	8.1. Install and Configure Mongoose  
+	8.2. Setup Campground Model  
+	8.3. Use Campground model inside our routes  
+9. Show Page  
+	9.1. Review the REST'ful Routes we've seen so far  
+	9.2. Add description to our campground model  
+	9.3. Show db.collection.drop()  
+	9.4. Add a show route/template  
+  
+	NOTE: The "/campgrounds/new" route should be defined before the "/campgrounds/:id" route, cause ":id" means anything, be it a number or "new".  
+   	
+==========v3==========	
+  
+10. Refactor Mongoose Code  
+	10.1. Create a models directory  
+	10.2. Use module.exports  
+	10.3. Require everything correctly!  
+11. Add Seeds file  
+	11.1. Add a seeds.js file  
+	11.2. Run the seeds file every time the server starts  
+12. Add the Comment Model  
+	12.1. Clear the errors  
+	12.2. Display comments on campgrounds show page  
+  
+==========v4==========  
+  
+13. Comments New/Create  
+	13.1. Discuss Nested Routes  
+	13.2. Add the comment new and create route  
+	13.3. Add the comment form  
+	
+	
---
+++ v4/app.js
@@ -0,0 +1,101 @@
+const express = require('express'),
+	  app = express(),
+	  bodyParser = require('body-parser'),
+	  mongoose = require('mongoose'),
+	  Campground = require('./models/campground'),
+	  seedDB = require('./seeds'),
+	  Comment = require('./models/comment');
+
+seedDB();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+mongoose.connect('mongodb://localhost:27017/yelpCamp', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}, function(error) {});
+
+app.get("/", (req, res) => {
+	res.render("landing");
+});
+
+// Index: Show all campgrounds
+app.get("/campgrounds", (req, res) => {
+	Campground.find({}, (err, allCampgrounds) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", {campgrounds : allCampgrounds});
+		}
+	});
+});
+// New: Show form to create new campground
+app.get("/campgrounds/new", (req, res) => {
+	res.render("campgrounds/new");
+})
+// Create: Add new campgrounds to the DB
+app.post("/campgrounds", (req, res) => {
+	// Get data from the form and add it to our array
+	// Redirect back to the campgrounds page
+	var name = req.body.name;
+	var image = req.body.image;
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};	
+	// Create a new Campground and save it to the DB
+	Campground.create(newCampground, (err, addedCampground) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");	
+		}
+	});
+});
+// Show: Displays details of a specific Campground
+app.get("/campgrounds/:id", (req, res) => {
+	// Find the Campground with provided ID
+	// Show template of that specific Campground
+	Campground.findById(req.params.id).populate("comments").exec( (err, foundCampground) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/show", {campground: foundCampground});	
+		}
+	});
+});
+// Comment Routes
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+	Campground.findById(req.params.id, (err, campground) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+	// Lookup campground using id
+	Campground.findById(req.params.id, (err, foundCampground) => {
+		if (err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			// Create new comment -> connect comment to the campground
+			// Redirect to campground show page
+			Comment.create(req.body.comment, (err, addedComment) => {
+				if (err) {
+					console.log(err);
+				} else {
+					foundCampground.comments.push(addedComment);
+					foundCampground.save();
+					res.redirect("/campgrounds/" + foundCampground._id);
+				}
+			});
+		}
+	});
+});
+
+app.listen(3000, ()=> {
+	console.log("YelpCamp Server has started...");
+});
